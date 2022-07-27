97d9a271f1d290b5970e61a5614cfadd7c3c148b
Telayna-I <telayna98@gmail.com>
2022-07-27 16:49:27 -0300

    API breaking bad

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './reset.css'
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+createRoot(
+  document.getElementById('root')
+).render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
